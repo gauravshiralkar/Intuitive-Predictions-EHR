fabdc8df6100cf1fdca9c8d3ca2d190cb4f5dad0
jinalijhaveri <jinalijhaveri@gmail.com>
2014-11-19 00:07:50 -0800

    Added CompanyReviews

--- model/dbConnection.js
+++ model/dbConnection.js
@@ -8,12 +8,23 @@ var connection = mysql.createConnection({
 	database : 'cmpe274'
 });
 
-exports.getCompanyRatings = function(callback) {
-	var query = "select companyName as name, companyGroup as 'group',overallRating,cultRatng,compsRating,currRatng,wrkLyfRatng from companysenti ";
+exports.getCompanyReviews = function(callback){
+	var query = 'select companyName as name,ratingDesc,overallRating as size from companysenti order by ratingDesc;'
+	
 	connection.query(query, function(err, rows) {
+			
+			callback(err, rows);
+	});
+			
+}
+
+exports.getCompanyRatings = function(callback) {
+	var query = "select companyName as name, companyGroup as 'group',overallRating as 'Overall Rating',cultRatng as 'Culture And Values Rating',compsRating as 'Compensation And Benefits Rating',currRatng,wrkLyfRatng as 'WorkLife Balance Rating' from companysenti ";
+	connection.query(query, function(err, rows) {
+		console.log(rows);
 		callback(err, rows);
 	});
-};
+}
 
 exports.getSalaryInfo = function(callback,q) {
 	if (q==="frontEnd or UI Developer") {q="frontEnd / UI Developer";}

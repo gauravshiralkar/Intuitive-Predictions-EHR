7fbd7acf3020f7fd163ed993da74b9fdc871c720
“gauravshiralkar” <gsatcoet@gmail.com>
2014-11-20 15:15:46 -0800

    Fixes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
 	host : 'localhost',
 	user : 'root',
     //password : '',
-  password : 'welcome',
+  password : 'pass',
 	port : '3306',
 	database : 'cmpe274'
 });

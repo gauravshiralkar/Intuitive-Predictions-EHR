6d26abb1fa43df193d4ef996b86be09a68c91303
minerva <dutta.minerva@gmail.com>
2014-11-20 15:53:54 -0800

    barcharts 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
 	host : 'localhost',
 	user : 'root',
     //password : '',
-  password : 'amma123',
+  password : '',
 	port : '3306',
 	database : 'cmpe274'
 });
--- model/dbConnection.js
+++ model/dbConnection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
 	host : 'localhost',
 	user : 'root',
 	//password : '',
-	password : 'amma123',
+	password : '',
 	port : '3306',
 	database : 'cmpe274'
 });

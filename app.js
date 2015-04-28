d20a12a680c07cff3a194eb1c980cf18fa0abf95
“gauravshiralkar” <gsatcoet@gmail.com>
2015-04-28 13:19:36 -0700

    Clean UI

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
 	host : 'localhost',
 	user : 'root',
     //password : '',
-  password : 'password123',
+  password : 'pass',
 	port : '3306',
 	database : 'cmpe239'
 });

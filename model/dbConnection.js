66e8ac8641709389ec7b3247422229b15bc21c34
deepna <bainsdeepna23@gmail.com>
2015-11-29 00:17:45 -0800

    Added Visualization dump

--- model/dbConnection.js
+++ model/dbConnection.js
@@ -12,8 +12,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host : 'localhost',
 	user : 'root',
-	//password : '',
-	password : 'pass',
+	password : '',
+	//password : 'pass',
 	port : '3306'
 });
 

f77488433aec8c4134e981657d3b3103dcf6af2f
Gaurav Shiralkar <gsatcoet@gmail.com>
2017-05-25 16:49:36 -0700

    Corrected for AWS

--- public/data/gaurav-typeahead.js
+++ public/data/gaurav-typeahead.js
@@ -2,45 +2,45 @@
 	$j('#pname').typeahead({
     	valueKey: 'pname',
         name: 'pname',
-        remote: 'http://localhost:3000/typeAhead/patientFullName/scratch?key=%QUERY',
+        remote: '/typeAhead/patientFullName/scratch?key=%QUERY',
         limit: 10 
     });
 	
 	$j('#paddress').typeahead({
     	valueKey: 'paddress',
         name: 'paddress',
-        remote: 'http://localhost:3000/typeAhead/patientAddressStreet/scratch?key=%QUERY',
+        remote: '/typeAhead/patientAddressStreet/scratch?key=%QUERY',
         limit: 10 
     });
 	
 	$j('#pcity').typeahead({
     	valueKey: 'pcity',
         name: 'pcity',
-        remote: 'http://localhost:3000/typeAhead/patientAddressCity/scratch?key=%QUERY',
+        remote: '/typeAhead/patientAddressCity/scratch?key=%QUERY',
         limit: 10 
     });
 	
 	$j('#pzip').typeahead({
     	valueKey: 'pzip',
         name: 'pzip',
-        remote: 'http://localhost:3000/typeAhead/patientAddressZip/scratch?key=%QUERY',
+        remote: '/typeAhead/patientAddressZip/scratch?key=%QUERY',
         limit: 10 
     });
 	$j('#diagkey').typeahead({
     	valueKey: 'diagcode',
         name: 'diagcode',
-        remote: 'http://localhost:3000/typeAhead/DiagnosticDesc/diagnostoprocedure?key=%QUERY',
+        remote: '/typeAhead/DiagnosticDesc/diagnostoprocedure?key=%QUERY',
         limit: 10 
     });
     $j('#treatkey').typeahead({
     	valueKey: 'treatcode',
         name: 'treatcode',
-        remote: 'http://localhost:3000/typeAhead/ProcedureDesc/diagnostoprocedure?key=%QUERY',
+        remote: '/typeAhead/ProcedureDesc/diagnostoprocedure?key=%QUERY',
         limit: 10 
     });
     $j('#providername').typeahead({
     	valueKey: 'providername',
         name: 'providername',
-        remote: 'http://localhost:3000/typeAhead/insuranceProviderName/scratch?key=%QUERY',
+        remote: '/typeAhead/insuranceProviderName/scratch?key=%QUERY',
         limit: 10 
     });
